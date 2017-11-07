bedfa5968d0532e34adbfe650a5e5aa724542a62
Sara Kennedy <slkennedy@gmail.com>
2017-11-07 11:04:30 -0500

    fixes broken link and js

--- scripts/site.js
+++ scripts/site.js
@@ -2,8 +2,10 @@ let header = document.querySelector(".willow-page-header");
 let open = document.querySelector(".willow-page-header__content-open");
 let close = document.querySelector(".willow-page-header__content-close");
 
-open.addEventListener("click", () => header.setAttribute("data-content-open", "true"));
-close.addEventListener("click", () => header.setAttribute("data-content-open", "false"));
+if(open) {
+    open.addEventListener("click", () => header.setAttribute("data-content-open", "true"));
+    close.addEventListener("click", () => header.setAttribute("data-content-open", "false"));
+}
 
 // declare dialog open, close and content
 let openDialogButton = document.querySelector("#openDialogButton");

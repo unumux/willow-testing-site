202c14e7bbc7f1a16801ab71c2e38a2ca44d1866
Sara Kennedy <slkennedy@gmail.com>
2017-11-07 10:57:16 -0500

    replaces home-nav with breadcrumbs. fixes js. attempts to update the icon path

--- scripts/site.js
+++ scripts/site.js
@@ -1,13 +1,35 @@
-// Main JS should go here!
-// Include scripts using Browserify by doing:
-// var $ = require("jquery");
-var header = document.querySelector(".willow-page-header");
-var open = document.querySelector(".willow-page-header__content-open");
-var close = document.querySelector(".willow-page-header__content-close");
+let header = document.querySelector(".willow-page-header");
+let open = document.querySelector(".willow-page-header__content-open");
+let close = document.querySelector(".willow-page-header__content-close");
 
-open.addEventListener("click", function(e) {
-    header.setAttribute("data-content-open", "true");    
-});
-close.addEventListener("click", function(e) {
-    header.setAttribute("data-content-open", "false");
-});
+open.addEventListener("click", () => header.setAttribute("data-content-open", "true"));
+close.addEventListener("click", () => header.setAttribute("data-content-open", "false"));
+
+// declare dialog open, close and content
+let openDialogButton = document.querySelector("#openDialogButton");
+let closeDialogButton = document.querySelector("#closeDialogButton");
+let dialog = document.querySelector(".willow-dialog");
+
+if(openDialogButton) {
+    // open the dialog content
+    openDialogButton.addEventListener("click", () => dialog.setAttribute("data-dialog-close", "false"));
+    
+    // close the dialog content
+    closeDialogButton.addEventListener("click", () => dialog.setAttribute("data-dialog-close", "true"));
+}
+    
+// declare modal open, close and content
+let openModalButton = document.querySelector("#openModalButton");
+let closeModalButton = document.querySelector("#closeModalButton");
+let closeModalButton2 = document.querySelector("#closeModalButton2");
+
+let modal = document.querySelector(".willow-modal");
+
+if(openModalButton) {
+    // opens the modal content
+    openModalButton.addEventListener("click", () => modal.setAttribute("data-modal-close", "false"));
+    
+    // closes the modal content
+    closeModalButton.addEventListener("click", () => modal.setAttribute("data-modal-close", "true"));
+    closeModalButton2.addEventListener("click", () => modal.setAttribute("data-modal-close", "true"));
+}
